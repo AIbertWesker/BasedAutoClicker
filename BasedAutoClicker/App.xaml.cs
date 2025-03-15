7e4993549e449ab9ffd00bb15eb472953f1d3645
AIbertWesker <mbubel999@gmail.com>
2025-03-15 14:40:14 +0100

    Add project files.

---
+++ BasedAutoClicker/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BasedAutoClicker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
